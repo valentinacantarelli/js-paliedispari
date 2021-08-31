98df43303ea32e8f9c9a555c3dc6e04d855bd470
valentina cantarelli <valentinacanterina@gmail.com>
2021-08-31 15:40:42 +0200

    aggiornato esercizio 1

--- palindromi/js/script.js
+++ palindromi/js/script.js
@@ -1,2 +1,22 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+
+function reverse(parola){
+    var nuovaParola = "";
+    for(var i = parola.length-1; i>=0;i--){
+       nuovaParola = nuovaParola+parola[i];
+    }
+    return nuovaParola;
+}
+
+var parolaUtente =prompt("Ciao, inserisci una parola");
+var parolaInversa = reverse(parolaUtente);
+
+if (parolaUtente == parolaInversa){
+    alert("La parola è palindroma");
+}else{
+    alert("La parola non è palindroma!")
+}
+
+
